f3c0f131e4f95fbc8da7802d2f81a25f33bcac82
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2012-11-02 11:29:28 +0000

    Allow using SendMessage from the command line.

--- src/SendMessage.cpp
+++ src/SendMessage.cpp
@@ -1,6 +1,6 @@
 // SendMessage - a tool to send custom messages
 
-// Copyright (C) 2010 - Stefan Kueng
+// Copyright (C) 2010, 2012 - Stefan Kueng
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -22,13 +22,125 @@
 #include "MainDlg.h"
 #include "AboutDlg.h"
 #include "CmdLineParser.h"
+#include "SmartHandle.h"
+#include <set>
+#include <Psapi.h>
 
+#pragma comment(lib, "Psapi.lib")
 #pragma comment(lib, "comctl32.lib")
 
 // Global Variables:
 HINSTANCE hInst;                                // current instance
+std::wstring windowTitle;
+std::wstring windowClass;
+std::wstring windowProcessname;
 
-// Forward declarations of functions included in this code module:
+BOOL CALLBACK ChildWindowEnumerator(HWND hwnd, LPARAM lParam)
+{
+    std::set<HWND> * pSet = (std::set<HWND>*)lParam;
+
+    if (!windowTitle.empty())
+    {
+        TCHAR buf[4096];
+        GetWindowText(hwnd, buf, 4096);
+        if (windowTitle.substr(0, 4096).compare(buf) != 0)
+            return TRUE;
+    }
+    if (!windowClass.empty())
+    {
+        TCHAR szClassName[100] = {0};
+        GetClassName(hwnd, szClassName, _countof(szClassName));
+        if (windowClass.compare(szClassName) != 0)
+            return TRUE;
+    }
+    if (!windowProcessname.empty())
+    {
+        DWORD pid = 0;
+        GetWindowThreadProcessId(hwnd, &pid);
+        if (pid)
+        {
+            CAutoGeneralHandle hProc = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, pid);
+            if (hProc)
+            {
+                TCHAR szProcName[MAX_PATH] = {0};
+                TCHAR * pProcName = szProcName;
+                if (GetProcessImageFileName(hProc, szProcName, MAX_PATH))
+                {
+                    pProcName = _tcsrchr(szProcName, '\\');
+                    if (pProcName)
+                        pProcName++;
+                    else
+                        pProcName = szProcName;
+                    if (windowProcessname.compare(pProcName) != 0)
+                        return TRUE;
+                }
+            }
+        }
+    }
+
+    pSet->insert(hwnd);
+
+    return TRUE;
+}
+
+BOOL CALLBACK WindowEnumerator(HWND hwnd, LPARAM lParam)
+{
+    std::set<HWND> * pSet = (std::set<HWND>*)lParam;
+
+    bool bMatches = true;
+    if (!windowTitle.empty())
+    {
+        TCHAR buf[4096];
+        GetWindowText(hwnd, buf, 4096);
+        if (windowTitle.substr(0, 4096).compare(buf) != 0)
+            bMatches = false;
+    }
+    if (!windowClass.empty())
+    {
+        TCHAR szClassName[100] = {0};
+        GetClassName(hwnd, szClassName, _countof(szClassName));
+        if (windowClass.compare(szClassName) != 0)
+            bMatches = false;
+    }
+    if (!windowProcessname.empty())
+    {
+        DWORD pid = 0;
+        GetWindowThreadProcessId(hwnd, &pid);
+        if (pid)
+        {
+            CAutoGeneralHandle hProc = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, pid);
+            if (hProc)
+            {
+                TCHAR szProcName[MAX_PATH] = {0};
+                TCHAR * pProcName = szProcName;
+                if (GetProcessImageFileName(hProc, szProcName, MAX_PATH))
+                {
+                    pProcName = _tcsrchr(szProcName, '\\');
+                    if (pProcName)
+                        pProcName++;
+                    else
+                        pProcName = szProcName;
+                    if (windowProcessname.compare(pProcName) != 0)
+                        bMatches = false;
+                }
+                else
+                    bMatches = false;
+            }
+            else
+                bMatches = false;
+        }
+        else
+            bMatches = false;
+    }
+    if (bMatches)
+        pSet->insert(hwnd);
+
+    if (windowProcessname.empty() || !windowTitle.empty() || !windowClass.empty())
+        EnumChildWindows(hwnd, ChildWindowEnumerator, lParam);
+
+    return TRUE;
+}
+
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
                      HINSTANCE hPrevInstance,
@@ -56,6 +168,37 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     {
         CAboutDlg aboutDlg(NULL);
         ret= aboutDlg.DoModal(hInstance, IDD_ABOUTBOX, NULL, NULL);
+    }
+    else if (parser.HasKey(L"message"))
+    {
+        UINT msg = parser.GetLongVal(L"message");
+        WPARAM wParam = parser.GetLongVal(L"wparam");
+        LPARAM lParam = (LPARAM)parser.GetLongLongVal(L"lparam");
+        bool bSend = !!parser.HasKey(L"send");
+
+        std::set<HWND> hwndset;
+
+        if (parser.HasVal(L"windowhandle"))
+            hwndset.insert((HWND)parser.GetLongLongVal(L"windowhandle"));
+        else
+        {
+            if (parser.HasVal(L"windowtitle"))
+                windowTitle = parser.GetVal(L"windowtitle");
+            if (parser.HasVal(L"windowclass"))
+                windowClass = parser.GetVal(L"windowclass");
+            if (parser.HasVal(L"processname"))
+                windowProcessname = parser.GetVal(L"processname");
+
+            EnumWindows(WindowEnumerator, (LPARAM)&hwndset);
+        }
+
+        for (auto it = hwndset.cbegin(); it != hwndset.cend(); ++it)
+        {
+            if (bSend)
+                ret = (int)SendMessage(*it, msg, wParam, lParam);
+            else
+                ret = (int)PostMessage(*it, msg, wParam, lParam);
+        }
     }
     else
     {
