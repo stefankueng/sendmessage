03821d41b6036a2a01284d5ba8e5124476de0886
XhmikosR <XhmikosR@126e4000-8520-4250-bc11-7c3da5379faf>
2012-11-26 18:15:55 +0000

    cleanup and consistency changes

--- trunk/src/SendMessage.cpp
+++ trunk/src/SendMessage.cpp
@@ -17,7 +17,7 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
 #include "stdafx.h"
-#include "Resource.h"
+#include "resource.h"
 #include "SendMessage.h"
 #include "MainDlg.h"
 #include "AboutDlg.h"
