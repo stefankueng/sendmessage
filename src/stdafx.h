6764be3d3102dcaa5c58c4cfd5839b4f68dcfcc0
huangqinjin <huangqinjin@gmail.com>
2022-07-16 17:31:43 +0800

    ShowWindow

--- src/stdafx.h
+++ src/stdafx.h
@@ -31,6 +31,7 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
+#include <windowsx.h>
 #include <oleacc.h>
 
 // C RunTime Header Files
